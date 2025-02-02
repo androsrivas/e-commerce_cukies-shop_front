d830377f15ce2027e4d74236479cab5bad1ef396
dustisme <androsrivas@outlook.com>
2025-02-02 11:12:28 +0100

    refactor product provider to better manage data

--- src/context/ProductContext/ProductProvider.jsx
+++ src/context/ProductContext/ProductProvider.jsx
@@ -1,14 +1,43 @@
-    import { ProductContext } from "./ProductContext";
-    import useProducts from "../../hooks/api/useProducts";
+import { ProductContext } from "./ProductContext";
+import useProductService from "../../hooks/api/useProductService";
+import { useState, useEffect } from "react";
 
-    const ProductProvider = ({ children }) => {
-        const { products, setProducts } = useProducts();
+const ProductProvider = ({ children }) => {
+    const { 
+        products,
+        loading,
+        errors, 
+        fetchAllProducts, 
+        fetchProductById, 
+        addProduct, 
+        modifyProduct, 
+        removeProduct 
+    } = useProductService();
+    const [ productList, setProductList ] = useState([]);
 
-        return(
-            <ProductContext.Provider value={{ products, setProducts }}>
-                { children }
-            </ProductContext.Provider>
-        );
-    };
+    useEffect(() => {
+        fetchAllProducts();
+    }, []);
 
-    export default ProductProvider;
+    useEffect(() => {
+        if (Array.isArray(products)) setProductList(products);
+    }, [products]);
+
+    return(
+        <ProductContext.Provider value={{
+            products,
+            setProductList,
+            loading,
+            errors,
+            fetchAllProducts,
+            fetchProductById,
+            addProduct,
+            modifyProduct,
+            removeProduct
+        }}>
+            { children }
+        </ProductContext.Provider>
+    );
+};
+
+export default ProductProvider;
