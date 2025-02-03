fb8a1b952818718f2098a0416817ea84e7ee2acc
dustisme <androsrivas@outlook.com>
2025-02-03 19:42:24 +0100

    create and implement custom hook useProductForm. works ok

--- src/components/organisms/forms/ProductForm.jsx
+++ src/components/organisms/forms/ProductForm.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import { FormProvider } from "react-hook-form";
 import {
     Form,
     FormControl,
@@ -11,10 +11,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 import FormButton from "../../atoms/buttons/FormButton";
-import { ProductContext } from "../../../context/ProductContext/ProductContext";
-import { yupResolver } from "@hookform/resolvers/yup";
 import { CategoryContext } from "../../../context/CategoryContext/CategoryContext";
-import productSchema from "../../../schemas/productSchema";
 import {
     Select,
     SelectContent,
@@ -22,104 +19,35 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select";
+import useProductForm from "../../../hooks/useProductForm";
 
 const ProductForm = () =>  {
-  const { addProduct } = useContext(ProductContext);
   const { categories } = useContext(CategoryContext);
-  const [ isSubmitting, setIsSubmitting ] = useState(false);
-  const form = useForm({
-    resolver: yupResolver(productSchema),
-    defaultValues: {
-      name: "",
-      price: 0,
-      description: "",
-      imageUrl: "",
-      categoryId: "Selecciona una categoría",
-      featured: false
-    },
-  });
+  const { form, onSubmit, isSubmitting } = useProductForm();
 
-  const onSubmit = async (values) => {
-    setIsSubmitting(true);
-    try {
-      const productData = {
-        ...values,
-        categoryId: values.categoryId,
-      };
-
-      await addProduct(productData);
-      form.reset();
-      alert("Producto creado correctamente.");
-    } catch (error) {
-      console.error("Error al crear el producto: ", error);
-      alert("Error al crear el producto.");
-    } finally {
-      setIsSubmitting(false);
-    }
-  }
+  const renderFormField = (label, name, Component, props = {}) => (
+    <FormField 
+      name={ name }
+      control={ form.control }
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{ label }</FormLabel>
+          <FormControl>
+            <Component {...field} {...props} />
+          </FormControl>
+        </FormItem>
+      )}
+    />
+  );
 
   return (
     <FormProvider>
     <Form {...form}>
       <form onSubmit={ form.handleSubmit(onSubmit) }>
-        <FormField
-          name="name"
-          control={ form.control }
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nombre</FormLabel>
-              <FormControl>
-                <Input placeholder="Nombre del producto" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField 
-          name="price"
-          control={ form.control }
-          render={({ field }) => (
-            <FormItem>
-          <FormLabel>Precio</FormLabel>
-            <FormControl>
-              <Input type="number" placeholder="Precio" {...field} />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-          )}
-        />
-          
-
-        <FormField 
-          name="description"
-          control={ form.control }
-          render={({ field }) => (
-            <FormItem>
-            <FormLabel>Descripción</FormLabel>
-              <FormControl>
-                <Input placeholder="Descripción" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        
-        
-        <FormField 
-          name="imageUrl"
-          control={ form.control }
-          render={({ field }) => (
-            <FormItem>
-            <FormLabel>Imagen</FormLabel>
-              <FormControl>
-                <Input type="url" placeholder="Enlace de imagen" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        
-
+        { renderFormField("Nombre", "name", Input, { placeholder: "Nombre del producto" }) }
+        { renderFormField("Precio", "price", Input, { type: "number", placeholder: "Precio"}) }
+        { renderFormField("Descripción", "description", Input, { placeholder: "Escribe aquí una breve descripción..."}) }
+        { renderFormField("Imagen", "imageUrl", Input, { type: "url", placeholder: "Enlace de imagen"}) }
         <FormField 
           name="categoryId"
           control={ form.control }
@@ -143,24 +71,13 @@ const ProductForm = () =>  {
           </FormItem>
           )}
         />
-            
-
-        <FormField 
-          name="featured"
-          control={ form.control }
-          render={({ field }) => (
-            <FormItem>
-            <FormLabel>Disponible</FormLabel>
-              <FormControl>
-                <Checkbox 
-                  id="featured" 
-                  checked={ !!field.value }
-                  onCheckedChange={ field.onChange }
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
+        
+        {renderFormField("Disponible", "featured", Checkbox, {
+          id: "featured",
+          checked: !!form.watch("featured"),
+          onCheckedChange: form.setValue.bind(null, "featured")
+        })}
+        
         <FormButton text="Crear" isSubmitting={ isSubmitting }/>
       </form>
     </Form>
